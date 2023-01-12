3657439af7d00ed8ee39fb35dd4bd992d1f28079
MiGer_tju <522794213@qq.com>
2023-01-12 11:40:55 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from PIL import Image
+from pylab import *
+
+
+"""Settings | Tools | Python Scientific | Show Plots in Toolwindow，去掉对勾"""
+""" 测试交互式标注
+im = array(Image.open('img.jpg'))
+imshow(im)
+print('please click 1 points')
+x = ginput(1)
+print(x)
+show()
+"""
+
+
+
+
